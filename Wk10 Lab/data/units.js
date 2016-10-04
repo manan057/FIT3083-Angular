a065db378b8b4c93b435057b62a311a880a0911c
Manan <manan_rana@hotmail.com>
2016-10-05 08:11:04 +1100

    organzied project directory

---
+++ Wk10 Lab/data/units.js
@@ -0,0 +1,22 @@
+var units = [
+    {
+        code: 'FIT1001',
+        title: 'Programming 1',
+        mark: 99
+    },
+    {
+        code: 'FIT1002',
+        title: 'Programming 2',
+        mark: 100
+    },
+    {
+        code: 'FIT1003',
+        title: 'Other Irrelevant topics',
+        mark: 50
+    },
+    {
+        code: 'FIT1004',
+        title: 'More Irrelevant Topics',
+        mark: 50
+    }
+];
